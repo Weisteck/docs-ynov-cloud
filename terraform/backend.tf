038873d32aef4da9a58684878ba0697feb768088
weisteck <hugo.celdran@gmail.com>
2023-07-06 10:59:50 +0200

    chore: only add site static

--- terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "archi-cloud"
-    key    = "tp-final.tfstate"
-    region = "eu-west-3"
-  }
-}
--- terraform/images.tf
+++
@@ -1,8 +0,0 @@
-resource "openstack_images_image_v2" "ubuntu" {
-  name             = "UbuntuOS"
-  container_format = "bare"
-  image_source_url = "https://cloud-images.ubuntu.com/jammy/20230602/jammy-server-cloudimg-amd64.img"
-  disk_format      = "qcow2"
-  min_disk_gb      = 3
-  min_ram_mb       = 1024
-}
